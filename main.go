e20dbd9fd29c9e33fc97716b8c6d0bf5a60e06a5
PerrySawatzky <sawatzky.perry@gmail.com>
2021-05-27 18:10:09 -0400

    figured out append, on my way to having it run all based on one battery being created sorry

--- main.go
+++ main.go
@@ -11,52 +11,102 @@ type Battery struct {
 	amountOfFloors            int
 	amountOfBasements         int
 	amountOfElevatorPerColumn int
+	columnList                []Column
+	floorRequestButtonList    []FloorRequestButton
 }
-func (e *Battery) Init(_id int, _amountOfColumns int, _amountOfFloors int, _amountOfBasements int, _amountOfElevatorPerColumn int){
+
+func trueFalseinator(i int) bool {
+	if i == 0 {
+		return true
+	} else {
+
+		return false
+	}
+}
+func (e *Battery) Init(_id int, _amountOfColumns int, _amountOfFloors int, _amountOfBasements int, _amountOfElevatorPerColumn int) {
 	e.ID = _id
 	e.amountOfColumns = _amountOfColumns
 	e.amountOfFloors = _amountOfFloors
 	e.amountOfBasements = _amountOfBasements
 	e.amountOfElevatorPerColumn = _amountOfElevatorPerColumn
-	for()
+
+	for i := 0; i < _amountOfColumns; i++ {
+		column := new(Column)
+		column.Init(i, _amountOfElevatorPerColumn, i, trueFalseinator(i))
+		e.columnList = append(e.columnList, *column)
+		if i == 0 {
+			for x := 0; x < 6; x++ {
+				//columnList[0].servedFloors.Add(x - 6) adds basements
+			}
+			//columnList[0].servedFloors.Add(1) adding lobby
+		}
+		if i == 1 {
+			for x := 0; x < 20; x++ {
+				//columnList[1].servedFloors.Add(x + 1)
+			}
+		}
+		if i == 2 {
+			for x := 20; x < 40; x++ {
+				//columnList[2].servedFloors.Add(x + 1)
+			}
+			//columnList[2].servedFloors.Add(1) adding lobby
+		}
+		if i == 3 {
+			for x := 40; x < 60; x++ {
+				//columnList[3].servedFloors.Add(x + 1)
+			}
+			//columnList[3].servedFloors.Add(1) adding lobby
+		}
+	}
+	//Lobby only up and down buttons
+	for i := 0; i < 1; i++ {
+		//upFloorRequestButtonCreator = new FloorRequestButton(i, i + 1, "up");
+		//floorRequestButtonsList.Add(upFloorRequestButtonCreator);
+	}
+	for i := 0; i < 1; i++ {
+		//downFloorRequestButtonCreator = new FloorRequestButton(i, i + 1, "down");
+		//floorRequestButtonsList.Add(downFloorRequestButtonCreator);
+	}
+
 }
+
 type Column struct {
 	ID                int
 	status            string
 	amountOfElevators int
 	servedFloors      int
 	isBasement        bool
-	elevatorList      int
-	callButtonList    int
+	elevatorList      []Elevator
+	callButtonList    []CallButton
 }
 
-func (e *Column) Init(_id int, _amountOfElevators int, _servedFloors int, _isBasement bool, elevatorList int, callButtonList int) {
+func (e *Column) Init(_id int, _amountOfElevators int, _servedFloors int, _isBasement bool) {
 	e.ID = _id
 	e.status = "built"
 	e.servedFloors = _servedFloors
 	e.isBasement = _isBasement
-	e.elevatorList = elevatorList
-	e.callButtonList = callButtonList
+	for i := 0; i < _amountOfElevators; i++ {
+		//elevator := new(Elevator)
+		//elevator.Init(i, "")
+	}
 }
+
 type Elevator struct {
 	ID                    int
 	status                string
 	currentFloor          int
 	direction             string
-	door                  int //figure this shit out later
-	floorRequestList      int
-	completedRequestsList int
+	floorRequestList      []int
+	completedRequestsList []int
+	door                  []Door //try making this a for loop
 }
 
-func (e *Elevator) Init(_id int, _status string, _currentFloor int, _direction string, _door int, _floorRequestList int, _completedRequestsList int) {
+func (e *Elevator) Init(_id int, _status string, _currentFloor int) {
 	e.ID = _id
 	e.status = _status
 	e.currentFloor = _currentFloor
-	e.direction = _direction
-	e.door = _door
-	e.floorRequestList = _floorRequestList
-	e.completedRequestsList = _completedRequestsList
 }
+
 type CallButton struct {
 	ID        int
 	floor     int
@@ -73,9 +123,12 @@ type Door struct {
 
 func main() {
 	elevator1 := new(Elevator)
-	elevator1.Init(1, "idle", 1, "", 1, 1, 1)
+	//elevator1.Init(1, "idle", 1, "")
 	column1 := new(Column)
-	column1.Init(1, 5, 0, false, 1, 1)
+	column1.Init(1, 5, 0, false)
 	fmt.Print(column1.ID, column1.amountOfElevators)
-	fmt.Print(emp.ID, emp.currentFloor)
+	fmt.Print(elevator1.ID, elevator1.currentFloor)
+	battery1 := new(Battery)
+	battery1.Init(1, 4, 60, 6, 5)
+	fmt.Print(battery1.columnList)
 }
